7e63a15aee6c0cd42530421a31c18ac27a309ba0
Christophe Delcourte <christophe.delcourte@gmail.com>
2013-12-19 10:27:32 +0100

    Master translation file addition.

---
+++ assets/templates/common/js/lang/english.lang.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$mil_lang_js['only_digits_are_allowed'] = "Only numbers are allowed.";
+$mil_lang_js['please_input_zipcode'] = "Please type your postal code.";
+$mil_lang_js['only_5_digits_in_zip'] = "For this country, your postal code must only have 5 numbers.";
+$mil_lang_js['only_4_digits_in_zip'] = "For this country, your postal code must only have 4 numbers.";
+$mil_lang_js['please_input_calling_code'] = "Please tupe the calling code.";
+$mil_lang_js['only_10_digits_in_phone'] = "Your phone number must have 10 numbers.";
+$mil_lang_js['please_select_a_country'] = "Please select a country.";
+$mil_lang_js['please_input_your_companyname'] = "Please type the name of your company.";
+$mil_lang_js['please_input_your_companynum'] = "Please type the identification number of your company.";
+$mil_lang_js['please_input_your_nationalnum'] = "Please type the french SIRET identification number of your company.";
+
+
+?>
